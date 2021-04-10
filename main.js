1a3b46f078fbfeaf9440ea84262eb7fa4f4f296e
Jakub Belicki <jakubsliwice@o2.pl>
2021-04-10 15:26:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let model = null;
-loadModel = async function(){
+let loadModel = async function(){
   model = await tf.loadLayersModel('https://raw.githubusercontent.com/kubzoey95/chorder/main/model.json');
   console.log("Model loaded!");
   console.log(model);
@@ -9,10 +9,12 @@ loadModel()
 
 let synth = null;
 
-loadSynth = async function(){
+let loadSynth = async function(){
   synth = new Synth().toMaster();
   console.log("Synth loaded!");
   console.log(synth);
 }
+
+loadSynth()
 
 // synth.triggerAttackRelease(Math.pow(2, (currNote.keyProps[0].int_value - 57) / 12) *440.0, 0.4, now)
