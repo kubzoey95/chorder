3b16480eecadca4597f3f0efa9702bbf1a9c26cb
Jakub Belicki <jakubsliwice@o2.pl>
2021-04-10 11:52:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-let model;
-xd = async function(){
+import "./Tone.js"
+
+let model = null;
+loadModel = async function(){
   model = await tf.loadLayersModel('https://raw.githubusercontent.com/kubzoey95/chorder/main/model.json');
   console.log("Model loaded!");
   console.log(model);
 }
 
-xd()
+loadModel()
+
+let synth = null;
+
+loadSynth = async function(){
+  await Tone.start();
+  synth = new Tone.Synth().toMaster();
+  console.log("Synth loaded!");
+  console.log(synth);
+}
+
+// synth.triggerAttackRelease(Math.pow(2, (currNote.keyProps[0].int_value - 57) / 12) *440.0, 0.4, now)
