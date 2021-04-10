4a30fffc9525dbd6a0c98d6e9db67c7b6203bf02
Jakub Belicki <jakubsliwice@o2.pl>
2021-04-10 21:36:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ $(document).keypress(async function(e){
     noteTensor.print();
     chordTensor.print();
     let prediction = model.predict([chordTensor, noteTensor]);
-    prediction.forEach(e => tf.argMax(e.reshape([13])));
+    prediction.forEach(e => tf.argMax(e.reshape([13])).print());
     
     synth && synth.triggerAttack(Math.pow(2, (tone + 3) / 12) * 440.0, now);
     currentTone = tone;
