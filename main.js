97dd60c5153837fc5e17c1282fa963ec97cbf5f2
Jakub Belicki <jakubsliwice@o2.pl>
2021-04-10 19:51:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const KEY_TONE_MAPPING = {       "2": 1,       "3": 3,               "5": 6,       "6": 8,       "7": 10,         // black keys
-                          "1": 0,       "w": 2,       "e": 4, "r": 5,       "t": 7,       "y": 9,        "u": 11} // white keys
+                          "q": 0,       "w": 2,       "e": 4, "r": 5,       "t": 7,       "y": 9,        "u": 11} // white keys
 
 let model = null;
 let loadModel = async function(){
