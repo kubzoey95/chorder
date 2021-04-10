061c5419bcf7ab5c136e0c04f8265e359b6716cf
Jakub Belicki <jakubsliwice@o2.pl>
2021-04-10 11:20:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-const model = await tf.loadLayersModel('https://raw.githubusercontent.com/kubzoey95/chorder/main/model.json');
+xd = async function(){
+  const model = await tf.loadLayersModel('https://raw.githubusercontent.com/kubzoey95/chorder/main/model.json');
 
-console.log(model);
+  console.log(model);
+}
+
+xd()
