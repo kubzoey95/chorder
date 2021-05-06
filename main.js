e8c075e7570de53aaa3e2c91be80e16915360d96
Jakub Belicki <jakubsliwice@o2.pl>
2021-05-06 16:31:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -88,16 +88,16 @@ engine.runRenderLoop(function () {
 	timeDelta = perf - time;
 	time = perf;
 	for (let note of noteStack){
-		note.x += timeDelta / 100;
+		note.x += timeDelta / 10;
 	}
 	if (noteStack.length > 0){
 		catmullRom = BABYLON.Curve3.CreateCatmullRomSpline(noteStack, 60, true);
 		catmullRomSpline = BABYLON.Mesh.CreateLines("catmullRom", catmullRom.getPoints(), scene);
-		scene.render();
 		if (noteStack.length > 0 && noteStack[noteStack.length - 1] > 300){
 			noteStack = noteStack.filter(e => e.x < 300);
 		}
 	}
+	scene.render();
 });
 
 // Watch for browser/canvas resize events
